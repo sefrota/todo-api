3bb872fb2f9ce907a70975f8101d208d7f56a4d1
Sergio Letras <sergio_letras@hotmail.com>
2017-03-18 17:35:51 +0000

    Add /todos and /todos/:id

--- server.js
+++ server.js
@@ -1,11 +1,49 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'Meet mom for lunch',
+	completed: false,
+}, {
+	id: 2,
+	description: 'Go to market',
+	completed:false
+}, {
+	id: 3,
+	description: 'Wash the car',
+	completed: false
+}];
+
+
 
 app.get('/', function(req, res){
 	res.send('TODO API Route');
 })
 
+//GET /todos
+app.get('/todos', function(req, res){
+	res.json(todos);
+});
+
+//GET /todos/:id
+app.get('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id, 10);
+	var foundTodo;
+	todos.forEach(function(todo){
+		if(todo.id === todoId){
+			foundTodo = todo;
+			return;
+		}
+	});
+	if(foundTodo)
+		res.json(foundTodo);
+	else
+		res.status(404).send();
+
+});
+
 app.listen(PORT, function(){
 	console.log('Express listening on port ' + PORT);
 });
+
