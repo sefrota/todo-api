ff2bb5fba08dc8e45ef0022c0f9ed6a75efd58a6
Sergio Letras <sergio_letras@hotmail.com>
2017-04-04 22:11:12 +0100

    Added salt and hash password functionalities

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.post('/users', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
 	db.user.create(body).then(function(user) {
-		res.json(user.toJSON());
+		res.json(user.toPublicJSON());
 	}).catch(function(e) {
 		res.status(400).json(e);
 	});
@@ -136,7 +136,7 @@ app.put('/todos/:id', function(req, res) {
 
 
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force:true}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT);
 	});
